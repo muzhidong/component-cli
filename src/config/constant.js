861acd29007b49108efd5e04848aa2097e1cdf4a
liuxiaodong <1119441533@qq.com>
2023-02-01 14:44:12 +0800

    feat: 删除config命令相关代码

--- src/config/constant.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // 公共组件路径前缀
-  COMPONENT_PATH_PREFIX: "src/common-components",
-  // 业务组件路径前缀
-  BUSINESS_COMPONENT_PATH_PREFIX: "src/business-components",
-  // 公共模板路径前缀
-  COMMON_TEMPLATE_PATH_PREFIX: "src/common-templates",
-}
